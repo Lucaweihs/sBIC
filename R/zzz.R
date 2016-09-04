5fc847fbbe4f05be98c511912fbfc0de4624e647
martyn <martyn.plummer@gmail.com>
2016-09-04 13:47:47 +0200

    Further changes to DESCRIPTION file. Remove poLCA hack.

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  # Do a messed up hack to fix the poLCA package.
-  fixPoLCA = ".Csave = .C; .C = function(...) { a = list(...); a$PACKAGE='poLCA'; do.call(.Csave, a)};"
-  trace("poLCA.probHat.C", tracer = parse(text = fixPoLCA), where = poLCA::poLCA, at = 1, print = F)
-  trace("poLCA.postClass.C", tracer = parse(text = fixPoLCA), where = poLCA::poLCA, at = 1, print = F)
-  trace("poLCA.ylik.C", tracer = parse(text = fixPoLCA), where = poLCA::poLCA, at = 1, print = F)
-}
